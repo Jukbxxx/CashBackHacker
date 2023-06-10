cca61aff271969b6bf8a2d57b743608d90bdcf9d
Maria Sarkisyan <mariia.sarkisyan@ya.ru>
2023-06-10 19:33:56 +0300

    Revert tests junit4 written"
    This reverts commit bec73eaee712836cb128cd332e05648b4cbebfc3.

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -3,32 +3,32 @@ package ru.netology.service;
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
-    @org.junit.Test
+    @org.testng.annotations.Test
     public void testCashbackHackServicePositive() {
         int actual = service.remain(1200);
         int expected = 800;
-        org.junit.Assert.assertEquals(expected, actual);
+        org.testng.AssertJUnit.assertEquals(expected, actual);
     }
 
-    @org.junit.Test
+    @org.testng.annotations.Test
     public void testCashbackHackServiceBoundary() {
         int actual = service.remain(1000);
         int expected = 0;
-        org.junit.Assert.assertEquals(expected, actual);
+        org.testng.AssertJUnit.assertEquals(expected, actual);
     }
 
-    @org.junit.Test
+    @org.testng.annotations.Test
     public void testCashbackHackServiceZeroAmount() {
         int actual = service.remain(0);
         int expected = 1000;
-        org.junit.Assert.assertEquals(expected, actual);
+        org.testng.AssertJUnit.assertEquals(expected, actual);
     }
 
-    @org.junit.Test
+    @org.testng.annotations.Test
     public void testCashbackHackServiceLargeAmount() {
         int actual = service.remain(900_300);
         int expected = 700;
-        org.junit.Assert.assertEquals(expected, actual);
+        org.testng.AssertJUnit.assertEquals(expected, actual);
     }
 
 }
