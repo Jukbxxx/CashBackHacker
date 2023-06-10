2fc62ba689985cad1bfe2e2143ebc004349dca67
Maria Sarkisyan <mariia.sarkisyan@ya.ru>
2023-06-10 18:32:07 +0300

    branch TestNG made

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,10 @@
+package ru.netology.service;
+public class CashbackHackServiceTest {
+
+//@org.testng.annotations.Test
+//    public void testCashbackHackService(){
+//    CashbackHackService service = new CashbackHackService();
+
+//}
+
+}
